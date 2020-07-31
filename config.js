b5e8adc187b5e80f2a04f46808590497070f3da1
Albert Zhang <alchzh@gmail.com>
2020-07-31 00:42:23 -0400

    safer config handling

--- config.js
+++ config.js
@@ -3,7 +3,11 @@ import fs from "fs";
 const config = JSON.parse(fs.readFileSync("./config.json"));
 
 fs.watchFile("./config.json", () => {
-  Object.assign(config, JSON.parse(fs.readFileSync("./config.json")));
+  try {
+    Object.assign(config, JSON.parse(fs.readFileSync("./config.json")));
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 export default config;
